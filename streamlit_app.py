39d3103efe8583575613350a21c73288b6e29949
Marco Valério <marcovalerio27j@gmail.com>
2025-06-23 21:22:07 -0300

    organização do grid dos gráficos e adição da tabela doc_sexo

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ from pathlib import Path
 
 st.set_page_config(
     page_title='Análise dos dados do Ensino Superior',
-    page_icon=':superhero:',
+    page_icon=':bar_chart:',
+    layout='wide'
 )
 
 @st.cache_data
@@ -17,6 +18,7 @@ def get_data(path):
 
 df_faixa_etaria = get_data('data/tabela_doc_faixa_etaria.csv')
 df_cor_raca = get_data('data/tabela_doc_cor_raca.csv')
+df_sexo = get_data('data/tabela_doc_sexo.csv')
 
 def pegar_frequencias(
     df: pd.DataFrame,
@@ -64,8 +66,12 @@ with st.sidebar:
 
 if ufs:
     df_faixa_etaria_filtrado = df_faixa_etaria[df_faixa_etaria["UF"].isin(ufs)]
+    df_cor_raca_filtrado = df_cor_raca[df_cor_raca["UF"].isin(ufs)]
+    df_sexo_filtrado = df_sexo[df_sexo["UF"].isin(ufs)]
 else:
     df_faixa_etaria_filtrado = df_faixa_etaria
+    df_cor_raca_filtrado = df_cor_raca
+    df_sexo_filtrado = df_sexo
 
 frequencia_faixa_etaria, frequencia_df_faixa_etaria = pegar_frequencias(
     df_faixa_etaria_filtrado,
@@ -73,6 +79,23 @@ frequencia_faixa_etaria, frequencia_df_faixa_etaria = pegar_frequencias(
     "Faixa Etária",
     "Frequência"
 )
+
+frequencia_cor_raca, frequencia_df_cor_raca = pegar_frequencias(
+    df_cor_raca_filtrado,
+    "FAIXA_ETARIA",
+    "Cor_Raca",
+    "Frequência"
+    )
+
+frequencia_sexo, frequencia_df_sexo = pegar_frequencias(
+    df_sexo_filtrado,
+    "SEXO",
+    "Sexo",
+    "Frequência"
+)
+
+frequencia_df_sexo['Percentual'] = frequencia_df_sexo['Frequência'] / frequencia_df_sexo['Frequência'].sum() * 100
+frequencia_df_sexo['Percentual_str'] = frequencia_df_sexo['Percentual'].map(lambda x: f"{x:.1f}%")
 
 faixa_etaria_barra = alt.Chart(frequencia_df_faixa_etaria).mark_bar(orient='horizontal').encode(
     x=alt.X("Frequência"),
@@ -85,10 +108,69 @@ faixa_etaria_barra = alt.Chart(frequencia_df_faixa_etaria).mark_bar(orient='hori
         anchor='middle',
         fontSize=20
     ),
+    height=835
+).interactive()
+
+cor_raca_barra = alt.Chart(frequencia_df_cor_raca).mark_bar(orient='vertical').encode(
+    x=alt.X("Cor_Raca", sort='-y', axis=alt.Axis(labelAngle=0)),
+    y=alt.Y("Frequência"),
+    tooltip=["Cor_Raca", "Frequência"],
+    color=alt.Color("Cor_Raca", legend=None)
+).properties(
+    title=alt.TitleParams(
+        text="Distribuição de Docentes por Cor e Raça",
+        anchor='middle',
+        fontSize=20
+    ),
 
     width=600,
     height=400
 ).interactive()
 
-st.altair_chart(faixa_etaria_barra, use_container_width=True)
+sexo_barra=alt.Chart(frequencia_df_sexo).mark_arc(innerRadius=100).encode(
+    theta=alt.Theta(field="Frequência", type="quantitative"),
+    color=alt.Color(field="Sexo", type="nominal", scale=alt.Scale(
+        domain=["FEM", "MASC"],
+        range=["#ff69b4", "#1f77b4"]
+    )),
+    tooltip=["Sexo", "Frequência"]
+).properties(
+    title=alt.TitleParams(
+        text="Distribuição de Docentes por Sexo",
+        anchor='middle',
+        fontSize=20
+    )
 
+).interactive()
+
+texto_central = alt.Chart(frequencia_df_sexo).mark_text(
+    text=f'{frequencia_df_sexo["Frequência"].sum()}', 
+    fontSize=50,
+    fontWeight='bold',
+    color='white'
+).encode(
+    x=alt.value(175),
+    y=alt.value(140)
+)
+
+texto_fatia = alt.Chart(frequencia_df_sexo).mark_text(
+    radius=120, size=13, fontWeight="bold", color="white"
+).encode(
+    theta=alt.Theta(field="Frequência", type="quantitative", stack="center"),
+    text='Percentual_str:N',
+    order=alt.Order('Frequência', sort='descending')
+)
+
+sexo_pizza = sexo_barra + texto_central + texto_fatia
+
+
+col_esquerda, col_direita = st.columns([1, 2], gap="large")
+
+with col_esquerda:
+    st.altair_chart(sexo_pizza, use_container_width=True)
+    st.markdown("---")
+    st.altair_chart(cor_raca_barra, use_container_width=True)
+    
+with col_direita:
+    st.altair_chart(faixa_etaria_barra, use_container_width=True)
+    
